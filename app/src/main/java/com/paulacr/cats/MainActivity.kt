929f5697158e5c8cc63e831448bc17ac6f1c92c3
Paula Rosa <paula.mecatronica@gmail.com>
2020-10-17 08:32:21 -0300

    Create MVVM basic structure

--- app/src/main/java/com/paulacr/cats/MainActivity.kt
+++ app/src/main/java/com/paulacr/cats/MainActivity.kt
@@ -2,11 +2,16 @@ package com.paulacr.cats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.paulacr.cats.ui.list.CatsListFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) 
+        setContentView(R.layout.activity_main)
+
+        // call fragment
+        supportFragmentManager.beginTransaction().replace(R.id.container, CatsListFragment())
+            .commit()
     }
 }
