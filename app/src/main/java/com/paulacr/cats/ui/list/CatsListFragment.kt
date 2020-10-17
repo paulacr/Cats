595d51a863de835da2ca477c37f2f3f22f351ce4
Paula Rosa <paula.mecatronica@gmail.com>
2020-10-17 08:59:53 -0300

    [Setup] Add layout to cats list fragment

--- app/src/main/java/com/paulacr/cats/ui/list/CatsListFragment.kt
+++ app/src/main/java/com/paulacr/cats/ui/list/CatsListFragment.kt
@@ -1,5 +1,12 @@
 package com.paulacr.cats.ui.list
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.paulacr.cats.R
 import com.paulacr.cats.databinding.FragmentCatsListBinding
 import com.paulacr.cats.ui.BaseFragment
 
@@ -7,4 +14,21 @@ class CatsListFragment : BaseFragment<CatsListViewModel, FragmentCatsListBinding
 
     override val viewModel: CatsListViewModel
         get() = CatsListViewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_cats_list, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        Log.i("Current Fragment ", fragmentName)
+    }
 }
