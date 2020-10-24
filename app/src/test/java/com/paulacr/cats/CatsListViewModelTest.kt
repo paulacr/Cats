c968f8e165720049ff5cd873ea82387c7c9df557
Paula Rosa <paula.mecatronica@gmail.com>
2020-10-24 15:15:04 -0300

    Fix view model tests

--- app/src/test/java/com/paulacr/cats/CatsListViewModelTest.kt
+++ app/src/test/java/com/paulacr/cats/CatsListViewModelTest.kt
@@ -1,12 +1,16 @@
 package com.paulacr.cats
 
+import com.paulacr.cats.data.model.CatImage
 import com.paulacr.cats.data.repository.CatRepository
 import com.paulacr.cats.ui.list.CatsListViewModel
+import io.reactivex.Single
+import io.reactivex.observers.TestObserver
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.mockito.Mockito.`when` as mockitoWhen
 
 class CatsListViewModelTest {
 
@@ -25,18 +29,31 @@ class CatsListViewModelTest {
     }
 
     @Test
-    fun loadSingleCat() {
+    fun showGetRandomCat() {
 
-//        mockitoWhen(repository.getRandomCat()).thenReturn(
-//            Single.just(CatImageResponse("1", "", listOf(), listOf()))
-//        )
-//
-//        val observable: Single<CatImageResponse> = repository.getRandomCat()
-//        val observer: TestObserver<CatImageResponse> = TestObserver()
-//        observable.subscribe(observer)
-//
-//        observer.assertComplete()
-//            .assertNoErrors()
-//            .assertValue(CatImageResponse("1", "", listOf(), listOf()))
+        val catImage = CatImage("1", "", emptyList(), emptyList())
+        mockitoWhen(repository.getRandomCat()).thenReturn(Single.just(catImage))
+
+        val observable: Single<CatImage> = repository.getRandomCat()
+        val observer: TestObserver<CatImage> = TestObserver()
+        observable.subscribe(observer)
+
+        observer.assertComplete()
+            .assertNoErrors()
+            .assertValue(CatImage("1", "", emptyList(), emptyList()))
+    }
+
+    @Test
+    fun shouldHandleErrorWhenRepositoryThrowsException() {
+
+        mockitoWhen(repository.getRandomCat()).thenReturn(
+            Single.error(Exception()))
+
+        val observable: Single<CatImage> = repository.getRandomCat()
+        val observer: TestObserver<CatImage> = TestObserver()
+        observable.subscribe(observer)
+
+        observer
+            .assertError(Exception::class.java)
     }
 }
--- app/src/test/java/com/paulacr/cats/data/repository/CatsListRepositoryTest.kt
+++ app/src/test/java/com/paulacr/cats/data/repository/CatsListRepositoryTest.kt
@@ -15,7 +15,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
-import org.mockito.Mockito.*
+import org.mockito.Mockito.never
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 import kotlin.NoSuchElementException
